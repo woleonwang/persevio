b002e6a6562c335327079452e9ecff86b423d08d
王民艺 <wangminyi@rcrai.com>
2024-12-25 03:46:50 +0800

    基础框架

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './pages/home/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/app' element={<App />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import styles from './style.module.less';
+const Home = () => {
+  return (
+    <div className={styles.b}>
+      <h1 className={styles.a}>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
