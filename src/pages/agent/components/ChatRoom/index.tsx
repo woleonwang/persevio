715f05535de26c04bc0ef70a1bec8805d24753fd
王民艺 <wangminyi@rcrai.com>
2025-01-28 12:40:35 +0800

    fix typo

--- src/pages/agent/components/ChatRoom/index.tsx
+++ src/pages/agent/components/ChatRoom/index.tsx
@@ -156,7 +156,7 @@ const ChatRoom: React.FC = () => {
           onCompositionEnd={() => {
             isCompositingRef.current = false;
           }}
-          onPressEnter={(e) => {
+          onPressEnter={() => {
             if (!isCompositingRef.current) {
               submit();
             }
