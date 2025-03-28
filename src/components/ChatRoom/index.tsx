93a4381a4c8214530eb8790efb9c29999adf7e77
王民艺 <115090665+woleonwang@users.noreply.github.com>
2025-03-29 00:01:35 +0800

    coworker survey

--- src/components/ChatRoom/index.tsx
+++ src/components/ChatRoom/index.tsx
@@ -233,7 +233,9 @@ const ChatRoom: React.FC<IProps> = (props) => {
         setMessages(messageHistory);
         if (role !== "candidate") {
           if (!!data.job.requirement_doc_id && !data.job.jrd_survey_opened_at) {
-            const { code } = await Post(`/api/jobs/${data.job.id}/open_survey`);
+            const { code } = await Post(
+              formatUrl(`/api/jobs/${data.job.id}/open_survey`)
+            );
             if (code === 0) {
               window.open(
                 "https://igk8gb3qpgz.sg.larksuite.com/wiki/Bf5DwwQLlixR12kY7jFl8qWPg2c?fromScene=spaceOverview&table=tblYl7ujQvy1Fj1F&view=vewYMhEF8Z",
