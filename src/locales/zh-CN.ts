1998c0469642df7720f8d15e88011236cf11b916
王民艺 <115090665+woleonwang@users.noreply.github.com>
2025-04-05 21:52:05 +0800

    中文翻译占位

--- src/locales/zh-CN.ts
+++ src/locales/zh-CN.ts
@@ -7,4 +7,55 @@ export default {
     company: "公司知识库",
     settings: "设置",
   },
+
+  job: {
+    chat: "Chat",
+    document: "Job Requirement Document",
+  },
+
+  chat: {
+    tips: "Tips: You can directly edit my responses! Modify summaries, add information, or answer my questions right after or below them.",
+    yes: "Yes.",
+    no: "No.",
+    accurate: "Accurate.",
+    proposal: "Your proposal is good.",
+    no_others: "No further modifications needed.",
+
+    create_job: "Open a new role",
+    define_job_requirement: "Define job requirements",
+    define_interview_plan: "Define interview plan",
+    draft_job_description: "Draft job description",
+    create_chatbot: "Create chatbot for candidate",
+  },
+
+  company: {
+    name: "Company Name",
+    knowledge_base: "Knowledge Base",
+    hint: {
+      title:
+        "Viona, your AI Recruiter, relies on the information in this database to perform key tasks:",
+      li1: "Understanding job requirements during conversations.",
+      li2: "Creating accurate and compelling job descriptions.",
+      li3: "Providing informed answers to candidate inquiries about the company.",
+      li4: "Many other tasks.",
+      footer:
+        "Please ensure all company details are comprehensive, accurate, and kept current. Incomplete or outdated information will impact Viona's effectiveness",
+    },
+  },
+
+  settings: {
+    profile: "Profile",
+    name: "Name",
+    email: "Email",
+    logout: "Log out",
+    change_password: "Change Password",
+  },
+
+  create_job: {
+    new_role: "Open a new role",
+    job_name: "Job Title",
+  },
+
+  save: "Save",
+  submit: "Submit",
 };
