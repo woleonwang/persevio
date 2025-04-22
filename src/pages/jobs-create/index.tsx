f66f5f1e188bf0fdae0a8f7bc2d31e1f2bb0269a
王民艺 <115090665+woleonwang@users.noreply.github.com>
2025-04-22 15:25:06 +0800

    防止同时创建多个 job

--- src/pages/jobs-create/index.tsx
+++ src/pages/jobs-create/index.tsx
@@ -4,10 +4,13 @@ import { useNavigate } from "react-router";
 import styles from "./style.module.less";
 import globalStore from "../../store/global";
 import { useTranslation } from "react-i18next";
+import { useRef } from "react";
 
 const JobCreate = () => {
   const [form] = Form.useForm();
   const { fetchJobs } = globalStore;
+
+  const isSubmittingRef = useRef(false);
 
   const navigate = useNavigate();
   const { t: originalT } = useTranslation();
@@ -16,6 +19,9 @@ const JobCreate = () => {
   const createJob = () => {
     form.validateFields().then(async (values) => {
       const { name } = values;
+      if (isSubmittingRef.current) return;
+
+      isSubmittingRef.current = true;
       const { code, data } = await Post("/api/jobs", {
         name: name,
       });
@@ -24,6 +30,7 @@ const JobCreate = () => {
         fetchJobs();
         navigate(`/app/jobs/${data.job_id}`);
       }
+      isSubmittingRef.current = false;
     });
   };
 
@@ -39,7 +46,12 @@ const JobCreate = () => {
           >
             <Input />
           </Form.Item>
-          <Button type="primary" onClick={createJob} style={{ marginTop: 24 }}>
+          <Button
+            type="primary"
+            onClick={createJob}
+            style={{ marginTop: 24 }}
+            disabled={isSubmittingRef.current}
+          >
             {originalT("submit")}
           </Button>
         </Form>
