33dbb491ff0f6b8866d5f6bf1537148e9629526b
王民艺 <115090665+woleonwang@users.noreply.github.com>
2025-07-24 19:51:37 +0800

    talent select 样式

--- src/pages/talent/select/index.tsx
+++ src/pages/talent/select/index.tsx
@@ -1,9 +1,12 @@
+import { ArrowLeftOutlined } from "@ant-design/icons";
 import { Avatar, Spin } from "antd";
 import { useNavigate, useParams } from "react-router";
 
 import useJob from "@/hooks/useJob";
 import VionaAvatar from "@/assets/viona-avatar.png";
 import SelectOrUploadTalent from "@/components/SelectOrUploadTalent";
+
+import styles from "./style.module.less";
 
 const TalentSelect = () => {
   const { job } = useJob();
@@ -25,19 +28,31 @@ const TalentSelect = () => {
   }
 
   return (
-    <div>
-      <div>{job.name}</div>
-
-      <div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <ArrowLeftOutlined
+          style={{
+            position: "absolute",
+            left: 0,
+            fontSize: 20,
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            navigate(`/app/jobs/${job.id}/board`);
+          }}
+        />
+        <div>{job.name}</div>
+      </div>
+      <div className={styles.body}>
         <div>
-          <Avatar src={VionaAvatar} />
-          <div>Viona</div>
-        </div>
-        <div>
+          <div className={styles.avatar}>
+            <Avatar src={VionaAvatar} />
+            <div>Viona</div>
+          </div>
           <div>
             您可以上传简历或选择一位候选人，与我一起制定该候选人的面试计划。
           </div>
-          <div>
+          <div style={{ marginTop: 20 }}>
             <SelectOrUploadTalent
               jobId={job.id}
               onChange={(val) => {
