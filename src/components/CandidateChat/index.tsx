9fda94392ca547e3c07ed7f3d568fd794af5e78a
王民艺 <115090665+woleonwang@users.noreply.github.com>
2025-09-24 12:05:39 +0800

    Update index.tsx

--- src/components/CandidateChat/index.tsx
+++ src/components/CandidateChat/index.tsx
@@ -341,7 +341,7 @@ Shall we start now?`,
             <div>您可以通过以下方式快速启用语音输入功能:</div>
             <ul className={styles.hintList}>
               <li>长按【Ctrl】键进行持续语音输入</li>
-              <li>连按两次【Ctrl】 键即可快速启动录音，再单次按下则结束录音入</li>
+              <li>连按两次【Ctrl】键快速启动录音，再次按下结束录音</li>
             </ul>
           </div>
         }
