3a66eb79f5f09c50b5fa72ca73c651f7fc909545
王民艺 <115090665+woleonwang@users.noreply.github.com>
2026-01-12 17:23:41 +0800

    初始化文档message id

--- src/components/StaffChat/index.tsx
+++ src/components/StaffChat/index.tsx
@@ -547,6 +547,7 @@ const StaffChat: React.FC<IProps> = (props) => {
             return !!extraTag;
           });
           autoTriggerTag?.handler?.(extraTag);
+          sideDocumentTriggerMessageIdRef.current = lastMessage.id;
         }
         lastMessageIdRef.current = lastMessage.id;
       }
