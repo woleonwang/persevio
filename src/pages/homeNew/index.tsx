e241c936ca94c75a450734f7a51f4ca702bdb1fb
王民艺 <115090665+woleonwang@users.noreply.github.com>
2026-01-27 11:39:01 +0800

    打开登录链接

--- src/pages/homeNew/index.tsx
+++ src/pages/homeNew/index.tsx
@@ -209,7 +209,7 @@ const HomeNew = () => {
                 type="primary"
                 size="large"
                 className={styles.ctaButton}
-                // onClick={() => navigate("/signin-candidate")}
+                onClick={() => navigate("/signin-candidate")}
               >
                 Chat with Viona now
               </Button>
@@ -448,7 +448,7 @@ const HomeNew = () => {
             </div>
             <div style={{ textAlign: "center" }}>
               <Button
-                // onClick={() => navigate("/signin-candidate")}
+                onClick={() => navigate("/signin-candidate")}
                 size="large"
                 className={styles.ctaButton}
               >
